a66f25f9da3f34009d353d47ee753003084f270a
krishna <krishnajatav504@gmail.com>
2024-06-04 01:11:20 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Home from './screens/Home';
 
 function App() {
   return (
-   <div>
-     
-   </div>
+    <>
+    <div>
+      <Home/>
+    </div>
+    </>
+   
   );
 }
 
