393925749098a3efec3c530b605851941446d711
krishna <krishnajatav504@gmail.com>
2024-06-06 23:05:26 +0530

    router implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 
 import './App.css';
 import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
     <div>
-      <Home/>
+     <Routes>
+      <Route exact path="/" element={<Home/>} />
+     </Routes>
     </div>
-    </>
+    </Router>
    
   );
 }
