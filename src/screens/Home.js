64764f8ee84bdf4dd3dea5cb54958370b8dbb67a
krishna <krishnajatav504@gmail.com>
2024-06-11 18:31:59 +0530

    Card component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
 
 export default function Home() {
   return (
@@ -8,26 +9,10 @@ export default function Home() {
       <div>
         <Navbar />
       </div>
-
-      <div>
-        <div className="card mt-3" style={{ width: "18rem", maxheight: "360px"  }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text </p>
-            <div className="container w-100">
-              <select className="m-2 h-100 w-100 bg-success">
-                {Array.from(Array(6),(e,i)=> {
-                  return(
-                    <option key={i+1} value ={i+1}>{i+1}</option>
-                  )
-                })}
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div> 
+      <Card />
+    </div>
+      
       <div>
         <Footer />
       </div>
