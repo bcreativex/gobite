479cea2cf188435d7bc2131790a2fa3724688f3d
krishna <krishnajatav504@gmail.com>
2024-06-26 23:35:26 +0530

    database connected

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://Krishnajatav:Krishna2322@cluster0.hhaxyt6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = 'mongodb+srv://Krishnajatav:Krishna2322@cluster0.hhaxyt6.mongodb.net/gobite?retryWrites=true&w=majority'
 const mongoDB =async() => {
-     await mongoose.connect(mongoURI,{useNewUrlParser:true},()=>{
-        console.log("connected");
+     await mongoose.connect(mongoURI,{useNewUrlParser: true},async(err,result)=>{
+        if(err) console.log("---",err) 
+        else{    
+            console.log("connected");
+            const fetched_data =  await mongoose.connection.db.collection("Krishna");
+            fetched_data.find({}).toArray(function( err,data){
+                 if(err) console.log(err);
+                 else console.log(data);
+            })
+        }
     });
 }
 
