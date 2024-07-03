8c6517688af54eb74e23791218ba0815460b2649
krishna <krishnajatav504@gmail.com>
2024-07-03 23:45:54 +0530

    url change

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -2,25 +2,25 @@ const express = require('express')
 const router = express.Router()
 const User  = require('../models/User')
 const { validationResult } = require('express-validator');
-router.post("/CreateUser", [
-    body('email').isEmail(),
-    body('name').optional().isLength({ min: 6 }),
-    body('password').optional().isLength({ min: 6 })]
-    ,async (req, res) =>{
+router.post("./createuser",[
+body('email').isEmail(),
+body('name').isLength({ min: 6 }),
+body('password').isLength({ min: 6 })],
+async (req, res) =>{
        
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array() });
         }
 
-    try {
-        User.create({
+        try {
+          await User.create({
             name: req.body.name,
             password:req.body.password,
             email: req.body.email,
             location:req.body.location
-        })
-    res.json({success:true});
+          })
+        res.json({success:true});
 
     } catch (error) {
         console.log(error)
--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://Krishnajatav:Krishna2322@cluster0.hhaxyt6.mongodb.net/gobite?retryWrites=true&w=majority'
+// const mongoURI = 'mongodb+srv://Krishnajatav:Krishna2322@cluster0.hhaxyt6.mongodb.net/gobite?retryWrites=true&w=majority'
+const mongoURI = 'mongodb://Krishnajatav:Krishna2322@ac-e2csb6n-shard-00-00.hhaxyt6.mongodb.net:27017,ac-e2csb6n-shard-00-01.hhaxyt6.mongodb.net:27017,ac-e2csb6n-shard-00-02.hhaxyt6.mongodb.net:27017/gobite?replicaSet=atlas-yp2086-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0' 
 const mongoDB =async() => {
      await mongoose.connect(mongoURI,{useNewUrlParser: true},async(err,result)=>{
         if(err) console.log("---",err) 
