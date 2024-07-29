45fc01c8e0c88f61cb782306386df3885cd52f9e
krishna <krishnajatav504@gmail.com>
2024-07-29 22:26:46 +0530

    add to card done

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,12 +9,29 @@ const priceRef = useRef();
   const [qty, setQty] = useState(1)
   const [size, setSize] = useState("")
   const handleAddToCart = async ()=> {
-    await dispatch({type:"ADD",id:props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty,size: size})
-    console.log(data)
+
+  let food =[]
+  for (const item of data) {
+    if(item.id === props.foodItem._id){
+      food =item;
+      break;
+    }
   }
   
+   if (food !== []) {
+    if (food.size === size) {
+      await dispatch ({type: "UPDATE", id: props.foodItem._id, price: finalPrice, qty:qty})
+      return
+    }
+    else if (food.size !== size) {
+      await dispatch({type:"ADD", id:props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty,size: size})
+      return
+    } // await console.log(data)
+   return
+  } 
+    await dispatch({type:"ADD",id:props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty,size: size})
+}
   let finalPrice = qty * parseInt(options[size]);
-
   useEffect(() => {
     setSize(priceRef.current.value)
   }, [])
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link ,useNavigate} from 'react-router-dom'
+import { Badge } from 'react-bootstrap';
+import Cart from '../screens/Cart';
+import Modal from '../Modal';
+import { useCart } from './ContextReducer';
 export default function Navbar() {
-  
+
+  const [cartView,setCartView] = useState(false)
+  let data = useCart();
   const navigate = useNavigate();
   const handleLogout = ()=> {
     localStorage.removeItem("authToken");
@@ -38,9 +44,11 @@ export default function Navbar() {
              </div>
               :
               <div>
-              <div className='btn bg-white text-success mx-2'>
-              MyCart
+              <div className='btn bg-white text-success mx-2' onClick={()=> {setCartView(true)}}>
+              MyCart {" "}
+              <Badge pill bg="danger" > {data.length} </Badge>
               </div>
+              {cartView? <Modal onClose={()=> setCartView(false)} ><Cart/></Modal> : null}
               <div className='btn bg-white text-danger mx-2' onClick={handleLogout}>
               Logout
               </div>
