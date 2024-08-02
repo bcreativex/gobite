da7f00069c6841b4fcc25f7e8243c446e9e5ecbf
krishna <krishnajatav504@gmail.com>
2024-08-02 23:29:42 +0530

    nothing

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -63,8 +63,6 @@ export default function MyOrder() {
 
                                                         </div>
 
-
-
                                                     }
 
                                                 </div>
