cee02cb988e6d3af1b862419eb363459b4120110
krishna <krishnajatav504@gmail.com>
2024-08-04 22:08:23 +0530

    img update

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -47,7 +47,7 @@ export default function MyOrder() {
 
                                                         <div className='col-12 col-md-6 col-lg-3' >
                                                             <div className="card mt-3 bg-dark" style={{ width: "16rem", maxHeight: "360px" }}>
-                                                                {/* <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} /> */}
+                                                                <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
                                                                 <div className="card-body text-light">
                                                                     <h5 className="card-title">{arrayData.name}</h5>
                                                                     <div className='container w-100 p-0' style={{ height: "38px" }}>
