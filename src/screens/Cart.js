28b3f59dc6c1757bbde9f4a186e0dd1e86d55619
krishna <krishnajatav504@gmail.com>
2024-08-17 18:20:06 +0530

    continue...

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -28,6 +28,10 @@ export default function Cart() {
         order_date:new Date().toDateString()
       })
     });
+    
+
+    // yaha se stripe ka kar
+    const stripe = await loadStripe("pk_test_51PmJHa049GpZfud4DVh2m5qUB2JcFmhPO24rD657ORP1FjLwMmyg6mCFW8ItJf6hDwcJ1MVwJ3XeUH1ZxGEwG1uo00Gd5stdvy");
 
     console.log("Order Response:" ,response)
     if (response.status===200) {
@@ -36,32 +40,6 @@ export default function Cart() {
 
   }
 
-  // const makePayment = async ()=> {
-  //   const stripe = await loadStripe("pk_test_51PmJHa049GpZfud4DVh2m5qUB2JcFmhPO24rD657ORP1FjLwMmyg6mCFW8ItJf6hDwcJ1MVwJ3XeUH1ZxGEwG1uo00Gd5stdvy");
-  //   const body = {
-  //     products: data
-  //   }
-
-  //   const headers = {
-  //      "Content-Type": "application/json"
-  //   }
-
-  //   const response = await fetch (`${apiURL}/create-checkout-session`, {
-  //     method:"POST",
-  //     headers:headers,
-  //     body:JSON.stringify(body)
-  //   })
-
-  //   const session  = await response.json();
-
-  //   const result = stripe.redirectToCheckout({
-  //     sessionId:session.id
-  //   });
-
-  //   if(result.error){
-  //     console.log(result.error);
-  //   }
-  // }
 
   let totalPrice = data.reduce((total,food) => total + food.price, 0)
   return (
