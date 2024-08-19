d2605257becb25abc39037122c1e45725782f8f5
krishna <krishnajatav504@gmail.com>
2024-08-19 23:50:19 +0530

    updated

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -14,6 +14,8 @@ export default function Cart() {
         </div>
      )
   }
+    
+  // main function
 
   const handleCheckOut  = async ()=> {
     let userEmail = localStorage.getItem("userEmail");
@@ -29,10 +31,6 @@ export default function Cart() {
       })
     });
     
-
-    // yaha se stripe ka kar
-    const stripe = await loadStripe("pk_test_51PmJHa049GpZfud4DVh2m5qUB2JcFmhPO24rD657ORP1FjLwMmyg6mCFW8ItJf6hDwcJ1MVwJ3XeUH1ZxGEwG1uo00Gd5stdvy");
-
     console.log("Order Response:" ,response)
     if (response.status===200) {
         dispatch({type:"DROP"})
@@ -40,6 +38,34 @@ export default function Cart() {
 
   }
 
+  const makePayment = async()=> {
+    const stripe = await loadStripe("pk_test_51PmJHa049GpZfud4DVh2m5qUB2JcFmhPO24rD657ORP1FjLwMmyg6mCFW8ItJf6hDwcJ1MVwJ3XeUH1ZxGEwG1uo00Gd5stdvy");
+
+    const body = {
+      products:data
+    }
+
+    const headers = {
+      "Content-Type": "application/json"
+    }
+
+    const response = await fetch ("http://localhost:5000/api/checkout",{
+      method:"POST",
+      headers:headers,
+      body:JSON.stringify(body)
+    });
+
+    const session = await response.json();
+
+    const result = stripe.redirectToCheckout({
+      sessionId:session.id
+    });
+
+    if (result.error){
+      console.log(result.error);
+    }
+
+  }
 
   let totalPrice = data.reduce((total,food) => total + food.price, 0)
   return (
@@ -72,7 +98,7 @@ export default function Cart() {
       </table>
       <div><h1 className='fs-2'>Total Price: {totalPrice}/-</h1></div>
       <div>
-        <button className='btn bg-info mt-5 ' onClick={handleCheckOut} > Check Out </button>
+        <button className='btn bg-info mt-5 ' onClick={()=>{handleCheckOut(); makePayment();}} > Check Out </button>
       </div>
     </div>
   </div>
